b0ff6f52536fd257c4c1e6287e58ce205e7c6f90
Camilo Torrado <deynertorrado1@gmail.com>
2024-06-11 21:55:36 -0500

    General changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Import routes
 const indexRoutes = require("./routes/index");
